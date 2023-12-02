4f4ab7967690cbdcd24dc84bc34086b32d76ba77
Alexey Simak <40754302+simakol@users.noreply.github.com>
2023-12-02 12:45:59 +0200

    lesson-5

---
+++ lesson-5/main.py
@@ -0,0 +1,37 @@
+# import random
+
+# def get_randon_int(min, max):
+#     return random.randint(min, max) # фукнція, яка генерує псевдовипадкове число в діапазоні від мінімального до максимального
+
+# print(get_randon_int(10, 100))
+# print(get_randon_int(500, 1000))
+# print(get_randon_int(-100, -50))
+
+
+import tkinter as tk # ми імпортуємо(підключаємо) бібліотеку текінтер, але називаємо її tk щоб менше писати слів
+
+def print_msg():
+    print("You clicked")
+
+
+window = tk.Tk() # це створення головного елементу інтерфейсу, а саме - вікна програми
+
+window.title("Це заголовок моєї програми!") # створює заголовок вікна програми
+
+window.geometry("400x200+450+300") # керування розміром і позицією нашого вікна. 400 це ширина, 200 - висота, 450 - це відступ зліва, а 300 - це відступ зверху
+
+window.configure(bg="blue") # змінює колір фону вікна
+
+greeting = tk.Label(text="Hello world!", bg="green", fg="black", font="Arial 50 bold") # це створення віджету для звичайного тексту
+
+greeting.pack()  # це відображення віджета тексту у вікні
+
+button = tk.Button(text="Click me", command=print_msg, bg="black", fg="green", font="Arial 30 bold", bd=0, padx="10", pady="8")
+
+button.pack()
+
+window.resizable(0, 0) # це заборона на зміну розміру екрану користувачеві
+
+
+
+window.mainloop() # це нескінченний цикл, який не дає нашому вікну закритись
