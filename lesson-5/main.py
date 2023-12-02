95e25640e19e136597f9cbb63d55c90dc0d73f18
Alexey Simak <40754302+simakol@users.noreply.github.com>
2023-12-02 14:04:49 +0200

    lesson-5

--- lesson-5/main.py
+++ lesson-5/main.py
@@ -4,16 +4,17 @@ import random
 def get_randon_int(min, max):
     return random.randint(min, max) # фукнція, яка генерує псевдовипадкове число в діапазоні від мінімального до максимального
 
-def print_msg():
-    print("You clicked")
-    greeting["text"] = "You already clicked!"
+def change_number():
+  min = int(minValue.get())
+  max = int(maxValue.get())
+  greeting["text"] = get_randon_int(min, max)
 
 
 window = tk.Tk() # це створення головного елементу інтерфейсу, а саме - вікна програми
 
 window.title("Це заголовок моєї програми!") # створює заголовок вікна програми
 
-window.geometry("400x200+450+300") # керування розміром і позицією нашого вікна. 400 це ширина, 200 - висота, 450 - це відступ зліва, а 300 - це відступ зверху
+window.geometry("400x200+150+50") # керування розміром і позицією нашого вікна. 400 це ширина, 200 - висота, 450 - це відступ зліва, а 300 - це відступ зверху
 
 window.configure(bg="blue") # змінює колір фону вікна
 
@@ -21,7 +22,16 @@ greeting = tk.Label(text="Hello world!", bg="green", fg="black", font="Arial 50 
 
 greeting.pack()  # це відображення віджета тексту у вікні
 
-button = tk.Button(text="Click me", command=print_msg, bg="black", fg="green", font="Arial 30 bold", bd=0, padx="10", pady="8")
+minValue = tk.StringVar() # це контейнер для збереженого тексту
+minEntry = tk.Entry(textvariable=minValue) #поле для вводу інформації
+
+maxValue = tk.StringVar() # це контейнер для збереженого тексту
+maxEntry = tk.Entry(textvariable=maxValue)
+
+minEntry.pack()
+maxEntry.pack()
+
+button = tk.Button(text="Click me", command=change_number, bg="black", fg="green", font="Arial 30 bold", bd=0, padx="10", pady="8")
 
 button.pack()
 
